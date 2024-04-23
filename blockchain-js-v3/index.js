c3363ed318de2ebcaf05bd9f09b34cd01558995f
mmjck <msantosjck@gmail.com>
2024-04-23 14:01:54 -0400

    new blockchain example

---
+++ blockchain-js-v3/index.js
@@ -0,0 +1,43 @@
+const Blockchain = require("./blockchain");
+
+const { send } = require("micro")
+
+const blockchain = new Blockchain()
+
+
+const run = async (req, res) => {
+    const route = req.url;
+    
+    
+    // Keep track of the peers that have contacted us
+    blockchain.addPeer(request.headers.host);
+
+    let output;
+
+    switch (route) {
+        case "/new_block":
+            output = blockchain.newBlock();
+            break;
+
+        case "/last_block":
+            output = blockchain.lastBlock();
+            break;
+
+        case "/get_peers":
+            output = blockchain.getPeers();
+            break;
+
+        case "/submit_transaction":
+            output = blockchain.addTransaction(transaction);
+            break;
+
+        default:
+            output = blockchain.lastBlock();
+
+    }
+
+    send(response, 200, output);
+
+}
+
+module.exports = run
